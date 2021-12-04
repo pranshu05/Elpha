64baed9d8f3e7195b068a64966747567b5f3fb36
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-04 20:45:55 +0530

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Interaction } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Pong!"),
+    async execute(interaction){
+        interaction.reply("Pong!")
+    }
+}
