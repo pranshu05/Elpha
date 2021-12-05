8c3e9797b0c58683aad2771f7705cb6905781caf
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-05 05:51:15 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,46 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Interaction } = require("discord.js");
+
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Pong!"),
-    async execute(interaction){
-        interaction.reply("Pong!")
-    }
-}
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Sends the bot\'s ping and latency'),
+	async execute(client, interaction) {
+		let ping = new MessageEmbed()
+        .setTitle(`Calculating ping...`)
+    
+        const startTime = Date.now();
+        interaction.reply({ embeds: [ping] })
+        .then(msg => {
+            const endTime = Date.now();
+            var pong = endTime-startTime;
+
+            let apipong = Math.round(client.ws.ping);
+
+            let emoj;
+
+            let colo;
+
+            if (apipong < 60) {
+              emoj = "<:3bar:908545977177214977>"
+              colo = "GREEN"
+            } else if (apipong > 60 && apipong < 100) {
+              emoj = "<:2bar:908546068038422628>"
+              colo = "ORANGE"
+            } else {
+              emoj = "<:1bar:908546024115695656>"
+              colo = "RED"
+            }
+    
+                let ping = new MessageEmbed()
+                .setTitle(`${emoj} Pong!`)
+                .setColor(colo)
+                .setDescription(`Latency: ${pong} ms \nApi Latency: ${apipong} ms` )
+    
+                interaction.editReply({ embeds: [ping] });
+		});
+	},
+};
