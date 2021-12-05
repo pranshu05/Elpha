1f8ced3d71e5b15fa359935ab4cf4b401e673dd6
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-05 08:44:00 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,40 +7,20 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Sends the bot\'s ping and latency'),
+		.setDescription('Sends the bot\'s ping '),
 	async execute(client, interaction) {
-		let ping = new MessageEmbed()
-        .setTitle(`Calculating ping...`)
+		
     
-        const startTime = Date.now();
-        interaction.reply({ embeds: [ping] })
-        .then(msg => {
-            const endTime = Date.now();
-            var pong = endTime-startTime;
+        
 
-            let apipong = Math.round(client.ws.ping);
 
-            let emoj;
-
-            let colo;
-
-            if (apipong < 60) {
-              emoj = "<:3bar:908545977177214977>"
-              colo = "GREEN"
-            } else if (apipong > 60 && apipong < 100) {
-              emoj = "<:2bar:908546068038422628>"
-              colo = "ORANGE"
-            } else {
-              emoj = "<:1bar:908546024115695656>"
-              colo = "RED"
-            }
-    
                 let ping = new MessageEmbed()
-                .setTitle(`${emoj} Pong!`)
-                .setColor(colo)
-                .setDescription(`Latency: ${pong} ms \nApi Latency: ${apipong} ms` )
+                .setTitle(`Pong!`)
+                .setColor('#00ffff')
+		.addField('🏓 Ping', `${(interaction.client.ws.ping).toFixed(0)} ms`, true)
+                
     
-                interaction.editReply({ embeds: [ping] });
+                interaction.Reply({ embeds: [ping] });
 		});
 	},
 };
