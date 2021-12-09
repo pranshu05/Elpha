d6d39505c404b4a2d68da82d43740e903fe015b7
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-09 06:08:34 +0530

    Add files via upload

---
+++ commands/anime.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const  Interaction  = require("discord.js");
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("anime")
+    .setDescription("Random anime's pic"),
+
+    async execute(interaction) {
+
+        const subreddits = [
+            'anime',
+            'goku',
+            'dragonball',
+            'uchiha',
+            
+        ];
+
+        const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
+			.then(response => response.json())
+			.then(body => body.data);
+		
+		const selected = data[Math.floor(Math.random() * data.length)];
+
+        const embed = new Discord.MessageEmbed()
+
+       .setColor('#00ffff')
+        .setImage(`https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`)
+        
+        interaction.reply({ embeds: [embed] })
+        
+    },
+    
+}
