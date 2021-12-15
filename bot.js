99cc2d97d704860769f08baf0cecc255021d2caa
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-15 17:32:00 +0530

    Done with issue of FadTheChad

--- bot.js
+++ bot.js
@@ -30,9 +30,9 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`)
     if (event.once){
-        client.once(event.name, (...args) => event.execute(client))
+        client.once(event.name, (...args) => event.execute(...args))
     } else {
-        client.on(event.name, (...args) => event.execute(client))
+        client.on(event.name, (...args) => event.execute(...args))
     }
     
 }
