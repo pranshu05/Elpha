66fcafa07ea90f155ea92ff64d2aa1a3910e66af
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-28 08:48:44 +0530

    e or p?

---
+++ commands/eatorpass.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const Discord = require('discord.js')
+const got = require('got')
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("eat-or-pass")
+    .setDescription("send eat or pass poll")
+      .addChannelOption(option => option
+			.setName("channel")
+			.setDescription("The channel to send the poll")
+			.setRequired(true)
+		),
+    async execute(interaction) {
+        const embed = new Discord.MessageEmbed()
+        got('https://www.reddit.com/r/food/random/.json')
+            .then(response => {
+                const [list] = JSON.parse(response.body)
+                const [post] = list.data.children
+                const permalink = post.data.permalink
+                const foodUrl = `https://reddit.com${permalink}`
+                const foodImage = post.data.url
+                const foodTitle = post.data.title
+                const foodUpvotes = post.data.ups
+                const foodNumComments = post.data.num_comments
+                embed.setTitle(`Eat or Pass`)
+                embed.setColor('#00FFFF')
+                embed.setImage(foodImage)
+                interaction.reply({ embeds: [embed] })
+                const message = await interaction.fetchReply()
+		            message.react(':regional_indicator_e:')
+		            message.react(':regional_indicator_p:')
+            })
+            .catch(console.error)
+    }
+}
