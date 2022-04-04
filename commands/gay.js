0c84617ca75598b2b63e6ce4dd4588ffe9540ef8
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-04-04 22:13:12 +0530

    gay

---
+++ commands/gay.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const Discord = require('discord.js')
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('gay')
+    .setDescription('gay someone')
+    .addUserOption(option =>
+        option.setName('user')
+            .setDescription('gay user')
+            .setRequired(true)
+    ),
+    async execute(interaction) {
+        var user = interaction.options.getUser('user')
+        if (user.id === '754381104034742415') {return interaction.reply('My developer isnt Gay')}
+        if (user === interaction.client.user) return interaction.reply('im not gay')
+        interaction.channel.send(`<@user.id> imagine being nerd`)
+    }
+}
