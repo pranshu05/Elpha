285b1b0b86dfbf9714be59ae0057448f9fe7b2fe
Indie <96850540+indiewastaken@users.noreply.github.com>
2022-11-26 05:49:16 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,47 +1,93 @@
 const General = require("../models/General")
-const fetch = require('node-fetch').default
-const Gif = require('../models/Gif')
+const Discord = require('discord.js')
 module.exports = {
     name: "messageCreate",
-    async execute(message, client){
-        const prefix = 'elp'
+    async execute(message ,client){
         const msg = message
-        const args = message.content.slice(prefix.length).trim().split(' ')
-        const command = args.shift().toLowerCase()
-        if(!msg) return
+        if(!message) return
         if (msg.author.bot) return
         const general = await General.findOne({guild_id: msg.guild.id})
-        const gif =  await Gif.find({guild_id: msg.guild.id})
-	if (message.content === `<@916613852362330133>`) {
-		await message.reply(`${message.author}, Don't ping :eyes:`).then(piing =>{
-			 message.reply(`${message.author}`)
-		})
-	}
-        if (command === 'gif') {
-          if(!gif){
-            return
-          }else{
-            if (!args.length) {
-              return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-            }else{
-              const name = await Gif.findOne({guild_id: msg.guild.id , gif_name: args[0]})
-              if(!name){
-                msg.reply(`No GIF found named `+ args[0])
-              }else{
-                const url = name.gif_url
-                msg.reply(url)
-              }
-            }
-          }
-        }
-		    if (!general) {
+		if (!general) {
             return
         }else{
+        question = ["why","when","where","whome", "which","How"],
+        greeeting = ["hi","hello","sup","whatsup","Hi","Hello","Sup","Whatsup"],
+        bye = ["bye","gtg","see u later","see u","seeya","gotta go"],
+        yes=["yes ","yeah ","ye","really","jokin","kidding"],
+        no=["no","nah","you know what"],
+        elpha =["Elpha","elpha"],
+        shut=["stfu","shut up","shut","shut the fuck up"],
+        bw=["fuck","bitch","shit","piss off","asshole","dick head","basterd","ass","dick"],
+        gn=["good night","gn"],
+        gm=["good morning","morning","gm"],
+        good=["good",'nice',"pog","excelent","genius","gud","nic"],
+        thanks=["thank","thx"],
+        lol=["lmao","lmfao","lol","rofl"],
+        ded=["ded chat","ded","dead","dead chat"],
+        have=["I have","have","i have","i hav"],
+        u=["u r","no u","you r","no you","you are"],
+        rick=['rick','rick roll','Rick','Rick rolled'],
+        ng=['never gonna','Never gonna','Never Gonna']
         if(msg.channel.id === general.general_channel_id){
-          fetch(`http://api.brainshop.ai/get?bid=163720&key=wN0HGDiinarW8Rle&uid=${message.author.id}&msg=${message.content.toLowerCase()}`).then(res => res.json()).then(json => {
-            const rep = json.cnt
-            msg.channel.send(rep)
-        }).catch(()=>{message.channel.send('API timeout')})
+          if (greeeting.some(word => msg.content.startsWith(word))) {
+            msg.reply('hello! How r you?')
+          }
+          if (question.some(word => msg.content.startsWith(word))) {
+            msg.reply('Why are u asking me?')
+          }
+          if (bye.some(word => msg.content.startsWith(word))) {
+            msg.reply('bye!')
+          }
+          if (yes.some(word => msg.content.startsWith(word))) {
+            msg.reply('no')
+          }
+          if (no.some(word => msg.content.startsWith(word))) {
+            msg.reply('yes')
+          }
+          if (elpha.some(word => msg.content.startsWith(word))) {
+            msg.reply('yes?')
+          }
+          if (shut.some(word => msg.content.includes(word))) {
+            msg.reply('you shut the fuck up')
+          }
+          if (bw.some(word => msg.content.includes(word))) {
+            msg.reply('mind your language mf')
+          }
+          if (gm.some(word => msg.content.startsWith(word))) {
+            msg.reply('Good Morning!')
+          }
+          if (gn.some(word => msg.content.startsWith(word))) {
+            msg.reply('Good Night!')
+          }
+          if (good.some(word => msg.content.includes(word))) {
+            msg.reply('thanks!')
+          }
+          if (thanks.some(word => msg.content.startsWith(word))) {
+            msg.reply('welcome!')
+          }
+          if (lol.some(word => msg.content.startsWith(word))) {
+            msg.reply('lmao')
+          }
+          if (ded.some(word => msg.content.startsWith(word))) {
+            msg.reply('Now Alive!')
+          }
+          if (u.some(word => msg.content.startsWith(word))) {
+            msg.reply('No you')
+          }
+          if (have.some(word => msg.content.startsWith(word))) {
+            msg.reply('I have money :sunglasses:')
+          }
+          if (rick.some(word => msg.content.includes(word))) {
+            msg.reply('Never Gonna Give You Up')
+          }
+          if (ng.some(word => msg.content.startsWith(word))) {
+            msg.reply('Give You Up')
+          }
+          if (message.mentions.has(client.user)) {
+            message.reply(`Don't ping me mf, ${message.author}`);
+          }
+        }else{
+          return
         }
     }
   }
