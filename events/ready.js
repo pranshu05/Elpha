277b36b36d16c09f260b2f1703a305dcd9186758
Pranshu Patel <pranshu05patel@gmail.com>
2023-01-20 17:01:01 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,45 +1,39 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-require("dotenv").config();
-
-const watching = [
-    "Pranshu developing me 👀",
-    `Over ${client.guilds.cache.size} servers`,
-    `Over ${client.guilds.cache.map((person) => person.memberCount).reduce((s, v) => s + (v || 0), 0)} users`,
-    "Pranshu developing me 👀",
-];
-
+const {REST} = require("@discordjs/rest")
+const {Routes} = require("discord-api-types/v9")
+require("dotenv").config()
 module.exports = {
     name: "ready",
     once: true,
-    execute(client, commands) {
-        console.log("Ready!");
-
-        // Set the bot's activity status
-        let index = 0;
+    execute(client, commands){
+        console.debug('ready!')
+        let watching = [
+            `Pranshu developing me 👀`,
+            `Over ${client.guilds.cache.size} servers`,
+            `Over ${client.guilds.cache.map(person => person.memberCount).reduce(function (s, v) { return s + (v || 0) }, 0)} users`,
+            `Pranshu developing me 👀`
+       ]
+        let index = 0
         setInterval(() => {
-            if (index === watching.length) index = 0;
-            const status = watching[index];
-            client.user.setStatus("dnd");
-            client.user.setActivity(status, { type: "WATCHING" });
-            index++;
-        }, 7000);
+        if(index === watching.length) index = 0
+        const status = watching[index]
+        client.user.setStatus('dnd')
+        client.user.setActivity(status, { type: "WATCHING"})
+        index++
+    }, 7000) 
+    const CLIENT_ID = client.user.id
+    const rest = new REST ({
+        version : "9"
+    }).setToken(process.env.token);
 
-        // Register commands globally
-        const CLIENT_ID = client.user.id;
-        const rest = new REST({ version: "9" }).setToken(process.env.token);
-        (async () => {
-            try {
-                await rest.put(Routes.applicationCommands(CLIENT_ID), {
-                    body: commands,
-                });
-                console.log(
-                    `Successfully registered commands globally with ${client.guilds.cache.map((person) => person.memberCount).reduce((s, v) => s + (v || 0), 0)} users and ${client.guilds.cache.size} servers`
-                );
-            } catch (err) {
-                console.error(err);
-            }
-        })();
-    },
-};
-
+    (async ()  => {
+        try{
+            await rest.put(Routes.applicationCommands(CLIENT_ID), {
+                body : commands
+            });
+            console.info(`Succesfully registered commands globally ! with ${client.guilds.cache.map(person => person.memberCount).reduce(function (s, v) { return s + (v || 0) }, 0)} users and ${client.guilds.cache.size} servers `)
+        } catch (err) {
+          if (err)  console.error(err)
+        }
+    })()
+    }
+}
