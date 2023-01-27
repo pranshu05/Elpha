05e6fa85c75b053215ffdeed5c4863a111673d59
Pranshu Patel <pranshu05patel@gmail.com>
2023-01-27 16:42:46 +0530

    hmm

---
+++ commands/guildlist.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("guild_list")
+    .setDescription("send list of guilds"),
+    async execute(interaction) {
+        if(interaction.user.id === '754381104034742415'){
+        interaction.reply('Done!')
+        console.log(interaction.client.guilds.cache
+            .map(g => `Guild Name: ${g.name}\n  Total Members: ${g.members.cache.size}\n Guild ID: ${g.id}`).join('\n\n')
+        ).catch(console.error)
+        }else {
+            interaction.reply('Insufficant Permissions')
+        }
+    }
+}
