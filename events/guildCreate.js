7310175c043f8b16c9c4f8e3ecc8d0f4c6c722a0
Pranshu Patel <pranshu05patel@gmail.com>
2023-01-27 18:21:31 +0530

    looool

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,29 @@
 const Discord = require('discord.js')
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 module.exports = {
 	name: "guildCreate",
 	async execute(guild,client) {
 		const channel = guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
+		if(!channel) return
+		const row = new MessageActionRow()
+			.addComponents(
+					new MessageButton()
+    				.setLabel('Github Repository')
+    				.setURL("https://github.com/pranshu05/elpha")
+    				.setStyle('LINK'),
+);
 		const embed = new Discord.MessageEmbed()
 			.setColor("00FFFF")
-			.setTitle("Hello I am Elpha")
-			.addField('Setup instructions', '1) Setup the welcome channel by `/setwelcomechannel` command! \n' + '2) Setup the general channel by `/setgeneralchannel` command!\n' + '3) Setup the modlog channel by `/setmodlogchannel` command\n' + '4) Setup the goodbye channel by `/setgoodbyechannel` command! \n' + '5) Setup autorole by `/setdefaultrole` command! \n')
-			.setDescription(`thanks for choosing me!`)
+			.setTitle("Hello I am Elpha,thanks for choosing me!")
+			.setDescription(`**I'm an open-source discord bot! Developed by contributors.Contribute to my code to Make me more powerful 💪🏻**`)
+			.addField('Setup instructions', '')
+			.addField('Setup Welcome Channel', '**Use Slashcommand**\n  >\`\`\`/setwelcomechannel\`\`\`')
+			.addField('Setup General Channel', '**Use Slashcommand**\n  >\`\`\`/setgeneralchannel\`\`\`')
+			.addField('Setup Modlogs Channel', '**Use Slashcommand**\n  >\`\`\`/setmodlogchannel\`\`\`')
+			.addField('Setup Goodbye Channel', '**Use Slashcommand**\n  >\`\`\`/setgoodbyechannel\`\`\`')
+			.addField('Create Default Role', '**Use Slashcommand**\n  >\`\`\`/setdefaultrole\`\`\`')
 			.setTimestamp()
-			channel.send({ embeds: [embed] }).catch((err) => console.log(err))
+			channel.send({ embeds: [embed], components: [row] }).catch((err) => console.log(err))
 			console.log(`Server joined: ${guild.name}`)
 	}
 }
