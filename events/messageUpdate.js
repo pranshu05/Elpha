b69210e2f0e3642e3e744585b33907af11b2e486
Pranshu Patel <pranshu05patel@gmail.com>
2023-01-29 09:02:50 +0530

    lol

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -6,7 +6,9 @@ module.exports = {
         if (!oldMessage.content) return
         const regex = /<@!?(1|\d{17,19})>/
         if (oldMessage.content.match(regex) || oldMessage.content.match('@everyone')) {
-           if(!newMessage.content.match(regex) || !newMessage.content.match('@everyone')) {
+           if(newMessage.content.match(regex) || newMessage.content.match('@everyone')) {
+                return
+            }
               console.log(`${oldMessage.author.username} updated ghost pinged message in ${oldMessage.channel} in ${oldMessage.guild}`)
               const embed = new Discord.MessageEmbed()
                 .setColor('RED')
@@ -17,8 +19,6 @@ module.exports = {
               oldMessage.channel.send({
                   embeds: [embed] 
               })
-             }
-              return
         }
     }
 }
