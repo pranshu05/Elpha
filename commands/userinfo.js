61eaece43d759ad5f63480b05d19e0f6042ceeeb
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-04 20:08:36 +0530

    b

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,28 +7,47 @@ module.exports = {
     .setDescription("sends user's info")
     .addUserOption(option =>
         option.setName('user')
-            .setDescription('user id')
+            .setDescription('Select a user')
             .setRequired(true)
     ),
     async execute(interaction) {
+        const member = interaction.options.getMember('user')
         const user = interaction.options.getUser('user')
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
+        let rolemap = member.roles.cache.sort((a, b) => b.position - a.position).map(r => r).join(" ")
+        if (rolemap.length > 1024) rolemap = "`The user has to many roles to display all of them!`"
+        if (!rolemap) rolemap = "`The user doesn't have any roles!`"
+        let status = {
+            online: 'Online',
+            idle: 'Idle',
+            dnd: 'Do Not Disturb',
+            offline: 'Offline/Invisible'
+        }
+        let status2 = {
+            true: 'Bot',
+            false: 'Not a Bot'
+        }
         const embed = new Discord.MessageEmbed()
        .setColor('#00ffff')
         .setTitle(`User info`)
         .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL())
         .setColor("00FFFF")
         .setThumbnail(user.displayAvatarURL())
-        .addField(`${user.tag}`, `${user}`, true)
-        .addField("ID:", `${user.id}`, true)
-        .addField("Nickname:", `${user.nickname !== null ? `${user.nickname}` : 'None'}`, true)
-        //.addField("Status:", `${user.presence.status}`, true)
-        .addField("In Server", interaction.guild.name, true)
-        //.addField("Permissons:", interaction.guild.members.cache.get(user).permissions)
-       // .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-        .addField("Bot:", `${user.bot}`, true)
-        .addField("Joined The Server On:", `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY')}`, true)
-        .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-        //
+        .addFields(
+        { name: 'Name', value: "`" + `${user.username}` + "`", inline: true }, 
+        { name: 'ID', value: "`" + `${user.id}` + "`", inline: true }, 
+        { name: 'Nickname', value: "`" + `${member.nickname || "No Nickname"}` + "`", inline: true }, 
+        { name: 'Bot', value: "`" + `${status2[user.bot]}` + "`", inline: true }, 
+        { name: 'Account Created', value: `<t:` + `${Math.floor(user.createdTimestamp/1000)}` + `:R>`, inline: true }, 
+        { name: 'Joined', value: `<t:` + `${Math.floor(member.joinedTimestamp/1000)}` + `:R>`, inline: true }, 
+        { name: 'Status', value: "`" + `${status[member.presence.status]}` + "`", inline: true }, 
+        { name: 'Server Boosting Since', value: "`" + `${member.premiumSince?.toLocaleDateString("en-US", options) || "Not Boosting"}` + "`", inline: true }, 
+        { name: 'Last Timeout', value: "`" + `${member.communicationDisabledUntil?.toLocaleDateString("en-US", options) || "Never Timed Out"}` + "`", inline: true }, 
+        { name: 'Voice Channel', value: `${member.voice.channel || "`Currently not in a Voice Channel`"}`, inline: true },
+		{ name: 'Roles', value: rolemap, inline: true },
+		{ name: 'Permissions', value: "`"+`${member.permissions.toArray()}`+"`", inline: false },
+	    )
+	    .setTimestamp()
         .setFooter(`Info of ${user.username}#${user.discriminator} requested by ${interaction.user.username}`)
         interaction.reply({ embeds: [embed] })
     }
