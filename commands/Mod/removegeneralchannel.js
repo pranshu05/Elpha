394f4c85fdc8e29e4a08d1e134b6f370216a16c3
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-06 12:17:04 +0000

    Main hmm

---
+++ commands/Mod/removegeneralchannel.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { Permissions} = require("discord.js")
+const Discord = require('discord.js') 
+const General = require("../../models/General")
+const Modlog = require("../../models/Modlog")
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("removegeneralchannel")
+		.setDescription("Remove the general message channel"),
+	async execute(interaction) {
+		const insf_perms = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+	    .setTitle(`**:x: Insufficient Permission!**`)
+        .setDescription(`You don't have permission to use this command.`)
+	   const gen_rem_embed = new Discord.MessageEmbed()
+	   .setColor('#00ff00')
+	   .setTitle(`**:white_check_mark: Removed General Channel**`)
+	   const gen_rem_db_fail = new Discord.MessageEmbed()
+	   .setColor('#FF0000')
+	   .setTitle(`**:x: DataBase Error!**`)
+	   .setDescription(`An error occurred while removing channel data from database!`)
+	   const modlog_perms = new Discord.MessageEmbed()
+	   .setColor('#FF0000')
+	   .setTitle(`**:x: Message Error!**`)
+	   .setDescription(`I don't have permission to send message in modlogs channel!`)
+		const modlog = await Modlog.findOne({guild_id: interaction.guild.id})
+		const channel = await General.find({guild_id: interaction.guild.id})
+		if (!interaction.member.permissions.has([ Permissions.FLAGS.MANAGE_CHANNELS , Permissions.FLAGS.MANAGE_MESSAGES , Permissions.FLAGS.MANAGE_ROLES , Permissions.FLAGS.ADMINISTRATOR ])) {
+			interaction.reply({embeds: [insf_perms]})
+			return
+		} 
+		if(channel.length === 0){
+			interaction.reply(`There isn't any general channel in this server!`)
+			return
+		}
+		General.deleteOne({ guild_id: interaction.guild.id }, (err, settings) => {
+			if (err) {
+				console.log(err)
+				interaction.reply({embeds: [gen_rem_db_fail]})
+				return
+			}
+			if (!settings) {
+				interaction.reply({embeds: [gen_rem_db_fail]})
+				return
+			} 
+			interaction.reply({embeds: [gen_rem_embed]})
+		})
+		if (!modlog) {
+			return
+		}else{
+			const abc = interaction.guild.channels.cache.get(modlog.modlog_channel_id)
+			if(!interaction.guild.me.permissionsIn(abc).has(Discord.Permissions.FLAGS.SEND_MESSAGES)){
+				if(interaction.guild.me.permissionsIn(interaction.channel).has(Discord.Permissions.FLAGS.SEND_MESSAGES)){
+					  interaction.channel.send({embeds: [modlog_perms]})
+					  return 
+				}
+				return 
+			}			
+				abc.send({embeds: [gen_rem_embed]})	
+		}
+	}
+}
---
+++ commands/NSFW/peepee.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const Discord = require('discord.js')
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('pp')
+    .setDescription('How long is your pp?'),
+    async execute(interaction) {
+        let pp = [
+            "You don't have a pp LMAO",
+            '8D',
+            '8=D',
+            '8==D',
+            '8===D',
+            '8====D',
+            '8=====D',
+            '8======D',
+            '8=======D',
+            '8========D',
+            'Oops its too large cannot fit in the message',
+        ]
+        let index = (Math.floor(Math.random() * Math.floor(pp.length)))
+        const embed = new Discord.MessageEmbed()
+        .setColor('#00FFFF')
+        .setTitle('Ratemachine PP')
+        .setDescription(`${interaction.member} has pp of size ${pp[index]}`)
+        .setFooter(`measured for ${interaction.user.username}`)
+        interaction.reply({ embeds: [embed] })
+    }
+}
