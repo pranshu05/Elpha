414b3fad0321eeffe9e9dd5ccaf195acd6afa534
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-07 19:04:58 +0530

    FRICK

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -8,7 +8,7 @@ module.exports = {
     .setDescription("WARNING! nsfw content 18+ only"),
   async execute(interaction) {
     got("https://www.reddit.com/r/Hornyjail/random.json?obey_over18=true")
-      .then(response => {
+      .then((response) => {
         const [list] = JSON.parse(response.body);
         const [post] = list.data.children;
         const permalink = post.data.permalink;
@@ -16,19 +16,17 @@ module.exports = {
         const media = post.data.media;
         let memeImage;
         if (media && media.type === "video") {
-          memeImage = media.reddit_video.fallback_url;
+          interaction.reply(media.reddit_video.fallback_url);
         } else if (media && media.type === "image") {
           memeImage = media.oembed.thumbnail_url;
-        } else {
-          memeImage = null;
+          const memeTitle = post.data.title;
+          const embed = new Discord.MessageEmbed()
+            .setTitle(memeTitle)
+            .setColor("#00FFFF")
+            .setImage(memeImage);
+          interaction.reply({ embeds: [embed] });
         }
-        const memeTitle = post.data.title;
-        const embed = new Discord.MessageEmbed()
-          .setTitle(memeTitle)
-          .setColor("#00FFFF")
-          .setImage(memeImage);
-        interaction.reply({ embeds: [embed] });
       })
       .catch(console.error);
-  }
+  },
 };
