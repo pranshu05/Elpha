0e3fc0206eb6c0b108d99a3e9207d68ac877954e
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-07 18:54:27 +0530

    hmm

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,25 +1,34 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const Discord = require("discord.js");
 const got = require("got");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("nsfw")
     .setDescription("WARNING! nsfw content 18+ only"),
   async execute(interaction) {
-    const embed = new Discord.MessageEmbed();
     got("https://www.reddit.com/r/Hornyjail/random.json?obey_over18=true")
-    .then(response => {
-      const [list] = JSON.parse(response.body)
-      const [post] = list.data.children
-      const permalink = post.data.permalink
-      const memeUrl = `https://reddit.com${permalink}`
-      const memeImage = post.data.url
-      const memeTitle = post.data.title
-      embed.setTitle(`${memeTitle}`)
-      embed.setColor('#00FFFF')
-      embed.setImage(memeImage)
-      interaction.reply({ embeds: [embed] })
+      .then(response => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
+        const permalink = post.data.permalink;
+        const memeUrl = `https://reddit.com${permalink}`;
+        const media = post.data.media;
+        let memeImage;
+        if (media && media.type === "video") {
+          memeImage = media.reddit_video.fallback_url;
+        } else if (media && media.type === "image") {
+          memeImage = media.oembed.thumbnail_url;
+        } else {
+          memeImage = null;
+        }
+        const memeTitle = post.data.title;
+        const embed = new Discord.MessageEmbed()
+          .setTitle(memeTitle)
+          .setColor("#00FFFF")
+          .setImage(memeImage);
+        interaction.reply({ embeds: [embed] });
       })
       .catch(console.error);
-  },
+  }
 };
