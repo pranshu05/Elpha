d5fd97fb449a144ef6ca149445b74f86433ebb63
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-07 18:29:32 +0530

    hmm

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,25 +1,26 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const Discord = require('discord.js')
-const got = require('got')
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js");
+const got = require("got");
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName("nsfw")
     .setDescription("WARNING! nsfw content 18+ only"),
-    async execute(interaction) {
-        const embed = new Discord.MessageEmbed()
-        got('https://www.reddit.com/r/Hornyjail/random.json?obey_over18=true')
-            .then(response => {
-                const [list] = JSON.parse(response.body)
-                const [post] = list.data.children
-                const nsfwImage = post.data.scrubber_media_url
-                const nsfwImage1 = post.data.url
-                const nsfwTitle = post.data.title
-                embed.setTitle(`${nsfwTitle}`)
-                embed.setColor('#00FFFF')
-                embed.setImage(nsfwImage)
-                embed.setImage(nsfwImage1)
-                interaction.reply({ embeds: [embed] })
-            })
-            .catch(console.error)
-    }
-}
+  async execute(interaction) {
+    const embed = new Discord.MessageEmbed();
+    got("https://www.reddit.com/r/Hornyjail/random.json?obey_over18=true")
+      .then((response) => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
+        const nsfwTitle = post.data.title;
+        embed.setTitle(`${nsfwTitle}`);
+        embed.setColor("#00FFFF");
+        if (post.data.fallback_url) {
+          embed.setImage(post.data.fallback_url);
+        } else {
+          embed.setImage(post.data.url);
+        }
+        interaction.reply({ embeds: [embed] });
+      })
+      .catch(console.error);
+  },
+};
