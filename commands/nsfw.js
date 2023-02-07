095092bd3a13879d7e38db50c9a75364bd96be4d
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-07 19:45:45 +0530

    Cool

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,6 +1,9 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const Discord = require('discord.js')
-const got = require('got')
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js");
+const got = require("got");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("nsfw")
@@ -10,17 +13,32 @@ module.exports = {
       .then((response) => {
         const [list] = JSON.parse(response.body);
         const [post] = list.data.children;
-        const media = post.data.media;
-        if (media && media.type === "video") {
-          return interaction.reply(post.data.media.reddit_video.fallback_url);
-        } else if (media && media.type === "image") {
-          const memeImage = post.data.url;
-          const memeTitle = post.data.title;
-          const embed = new Discord.MessageEmbed()
-            .setTitle(memeTitle)
-            .setColor("#00FFFF")
-            .setImage(memeImage);
+        const permalink = post.data.permalink;
+        const memeUrl = `https://reddit.com${permalink}`;
+        const memeImage = post.data.url;
+        const memeTitle = post.data.title;
+        const memeUpvotes = post.data.ups;
+        const memeNumComments = post.data.num_comments;
+        const extension = path.extname(memeImage);
+        if (
+          extension === ".jpg" ||
+          extension === ".png" ||
+          extension === ".jpeg"
+        ) {
+          const embed = new Discord.MessageEmbed();
+          embed.setTitle(`${memeTitle}`);
+          embed.setColor("#00FFFF");
+          embed.setImage(memeImage);
+          embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
           interaction.reply({ embeds: [embed] });
+        } else if (extension === ".mp4" || extension === ".gif") {
+          interaction.reply(
+            `${memeTitle}\n${memeImage}\n👍 ${memeUpvotes} 💬 ${memeNumComments}`
+          );
+        } else {
+          interaction.reply(
+            `Sorry, the content format is not supported. Please retry`
+          );
         }
       })
       .catch(console.error);
