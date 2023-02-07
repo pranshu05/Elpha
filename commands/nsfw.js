d483b8f067b983b47fe130c3a4e444128b8c40d0
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-07 23:38:02 +0530

    hmm

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -9,23 +9,22 @@ module.exports = {
     .setName("nsfw")
     .setDescription("WARNING! nsfw content 18+ only"),
   async execute(interaction) {
-    got("https://www.reddit.com/r/Hornyjail/top.json?include_over_18=on")
+    got("https://www.reddit.com/r/Hornyjail/random.json?include_over_18=on")
       .then((response) => {
         const [list] = JSON.parse(response.body);
         const [post] = list.data.children;
         const permalink = post.data.permalink;
-        const memeUrl = `https://reddit.com${permalink}`;
-        const memeImage = post.data.url;
-        const memevideo = post.data.secure_media.reddit_video.fallback_url ;
+        const memeImage = post.data.url ;
+        const memevideo = post.data.url + '/DASH_240.mp4' ;
         const memeTitle = post.data.title;
         const memeUpvotes = post.data.ups;
         const memeNumComments = post.data.num_comments;
         const extension = path.extname(memeImage);
         if (extension === ".jpg" || extension === ".png" || extension === ".jpeg") {
           const embed = new Discord.MessageEmbed();
-          embed.setTitle(`${memeTitle}`);
+          embed.setTitle(memeTitle);
           embed.setColor("#00FFFF");
-          embed.setImage(`${memeImage}`);
+          embed.setImage(memeImage);
           embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
           interaction.reply({ embeds: [embed] });
         } else {
