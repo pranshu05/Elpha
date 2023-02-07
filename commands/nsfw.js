fc2078cf0ae8113ed70c1ac7164b647b96982c4a
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-07 18:42:06 +0530

    idk

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -8,18 +8,17 @@ module.exports = {
   async execute(interaction) {
     const embed = new Discord.MessageEmbed();
     got("https://www.reddit.com/r/Hornyjail/random.json?obey_over18=true")
-      .then((response) => {
-        const [list] = JSON.parse(response.body);
-        const [post] = list.data.children;
-        const nsfwTitle = post.data.title;
-        embed.setTitle(`${nsfwTitle}`);
-        embed.setColor("#00FFFF");
-        if (post.data.secure_media.reddit_video.fallback_url) {
-          embed.setImage(post.data.secure_media.reddit_video.fallback_url);
-        } else {
-          embed.setImage(post.data.url);
-        }
-        interaction.reply({ embeds: [embed] });
+    .then(response => {
+      const [list] = JSON.parse(response.body)
+      const [post] = list.data.children
+      const permalink = post.data.permalink
+      const memeUrl = `https://reddit.com${permalink}`
+      const memeImage = post.data.url
+      const memeTitle = post.data.title
+      embed.setTitle(`${memeTitle}`)
+      embed.setColor('#00FFFF')
+      embed.setImage(memeImage)
+      interaction.reply({ embeds: [embed] })
       })
       .catch(console.error);
   },
