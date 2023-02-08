4677cd317c71d19d8ea5db8b200edb6ca236a0dd
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-08 12:18:54 +0530

    finger crossed

--- commands/nsfwmeme.js
+++ commands/nsfwmeme.js
@@ -1,33 +1,36 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const Discord = require("discord.js")
-const got = require("got")
-const fs = require("fs")
-const path = require("path")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js");
+const got = require("got");
+const fs = require("fs");
+const path = require("path");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("nsfwmeme")
     .setDescription("WARNING! nsfw(meme) content 18+ only"),
-    async execute(interaction) {
-    if (!interaction.channel.nsfw) {
-          return interaction.reply('You can use this commands only in NSFW channel!')
-  } else {
-    got("https://www.reddit.com/r/NSFWMemes/random.json?include_over_18=on")
-     .then(response => {
-                const [list] = JSON.parse(response.body)
-                const [post] = list.data.children
-                const permalink = post.data.permalink
-                const memeUrl = `https://reddit.com${permalink}`
-                const memeImage = post.data.url
-                const memeTitle = post.data.title
-                const memeUpvotes = post.data.ups
-                const memeNumComments = post.data.num_comments
-                embed.setTitle(`${memeTitle}`)
-                embed.setColor('#00FFFF')
-                embed.setImage(memeImage)
-                embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`)
-                interaction.reply({ embeds: [embed] })
-            })
-            .catch(console.error)
+  async execute(interaction) {
+    if (interaction.channel.nsfw) {
+      got("https://www.reddit.com/r/NSFWMemes/random.json?include_over_18=on")
+        .then((response) => {
+          const [list] = JSON.parse(response.body);
+          const [post] = list.data.children;
+          const permalink = post.data.permalink;
+          const memeUrl = `https://reddit.com${permalink}`;
+          const memeImage = post.data.url;
+          const memeTitle = post.data.title;
+          const memeUpvotes = post.data.ups;
+          const memeNumComments = post.data.num_comments;
+          let embed = new Discord.MessageEmbed();
+          embed.setTitle(`${memeTitle}`);
+          embed.setColor("#00FFFF");
+          embed.setImage(memeImage);
+          embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+          interaction.reply({ embeds: [embed] });
+        })
+        .catch(console.error);
+    } else {
+      return interaction.reply(
+        "You can use this command only in NSFW channel!"
+      );
     }
-  }
-}
+  },
+};
