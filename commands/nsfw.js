8f9d65d2cc5f20a2612875af7aa2849a37ded137
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-08 10:05:43 +0530

    tenor

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,38 +1,31 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const Discord = require("discord.js");
-const got = require("got");
-const fs = require("fs");
-const path = require("path");
-
+require("dotenv").config()
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("nsfw")
     .setDescription("WARNING! nsfw content 18+ only"),
   async execute(interaction) {
-    got("https://www.reddit.com/r/Hornyjail/random.json?include_over_18=on")
-      .then((response) => {
-        const [list] = JSON.parse(response.body);
-        const [post] = list.data.children;
-        const permalink = post.data.permalink;
-        const memeImage = post.data.url ;
-        const memevideo = post.data.url + '/DASH_240.mp4' ;
-        const memeTitle = post.data.title;
-        const memeUpvotes = post.data.ups;
-        const memeNumComments = post.data.num_comments;
-        const extension = path.extname(memeImage);
-        if (extension === ".jpg" || extension === ".png" || extension === ".jpeg") {
-          const embed = new Discord.MessageEmbed();
-          embed.setTitle(memeTitle);
-          embed.setColor("#00FFFF");
-          embed.setImage(memeImage);
-          embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-          interaction.reply({ embeds: [embed] });
-        } else {
-          interaction.reply(
-            `${memeTitle} \n ${memevideo}`
-          );
-        }
-      })
-      .catch(console.error);
-  },
-};
+       let keywords = [
+          'horny jail',
+          'no horny',
+          'anti horny',
+       ]
+       let keyword = (Math.floor(Math.random() * Math.floor(keywords.length)))
+       let sentances = [
+          'What were you expecting?',
+          'You horny jerk!',
+          'I will send you to Jesus',
+          'Go to horny jail',
+          'Jesus is knocking on your door',
+          'I am sending FBI',
+       ]
+       let sentance = (Math.floor(Math.random() * Math.floor(sentances.length)))
+       let url = `https://g.tenor.com/v1/search?q=${keyword}&key=${process.env.tenor}&limit=8`
+       let response = await fetch(url)
+       let json = await response.json()
+       let random_url = Math.floor(Math.random() * json.results[0].length)
+       interaction.reply(`LMAO ${interaction.user}, ${sentance} \n json.results[random_url].url`)
+  }
+}
