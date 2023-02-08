40f794c1ff7c13dbf4552c7eeaba7d7d231b4712
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2023-02-08 13:00:28 +0530

    hmm

--- commands/nsfwmeme.js
+++ commands/nsfwmeme.js
@@ -9,23 +9,31 @@ module.exports = {
     .setDescription("WARNING! nsfw(meme) content 18+ only"),
   async execute(interaction) {
     if (interaction.channel.nsfw) {
-      got("https://www.reddit.com/r/NSFW_GIF/random.json")
-         .then(response => {
-                const [list] = JSON.parse(response.body)
-                const [post] = list.data.children
-                const permalink = post.data.permalink
-                const memeUrl = `https://reddit.com${permalink}`
-                const memeImage = post.data.url
-                const memeTitle = post.data.title
-                const memeUpvotes = post.data.ups
-                const memeNumComments = post.data.num_comments
-                embed.setTitle(`${memeTitle}`)
-                embed.setColor('#00FFFF')
-                embed.setImage(memeImage)
-                embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`)
-                interaction.reply({ embeds: [embed] })
-            })
-        .catch(console.error);
+       got("https://www.reddit.com/r/Hornyjail/random.json?include_over_18=on")
+      .then((response) => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
+        const permalink = post.data.permalink;
+        const memeImage = post.data.url ;
+        const memevideo = post.data.url + '/DASH_240.mp4' ;
+        const memeTitle = post.data.title;
+        const memeUpvotes = post.data.ups;
+        const memeNumComments = post.data.num_comments;
+        const extension = path.extname(memeImage);
+        if (extension === ".jpg" || extension === ".png" || extension === ".jpeg") {
+          const embed = new Discord.MessageEmbed();
+          embed.setTitle(memeTitle);
+          embed.setColor("#00FFFF");
+          embed.setImage(memeImage);
+          embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+          interaction.reply({ embeds: [embed] });
+        } else {
+          interaction.reply(
+            `${memeTitle} \n ${memevideo}`
+          );
+        }
+      })
+      .catch(console.error);
     } else {
       return interaction.reply(
         "You can use this command only in NSFW channel!"
