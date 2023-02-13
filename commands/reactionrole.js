2261e7e9658cdcd36f332aa85f6f6cd47beda801
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-13 17:57:51 +0530

    ok done

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const  Interaction  = require("discord.js")
 const Discord = require('discord.js')
-const {Client, Intents, MessageActionRow, MessageSelectMenu } = require('discord.js')
+const {MessageActionRow,  MessageButton,  ButtonInteraction, MessageSelectMenu, } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("reaction_roles")
@@ -11,45 +11,43 @@ module.exports = {
         .setColor('#FF0000')
 	    .setTitle(`**:x: Insufficient Permission!**`)
         .setDescription(`You don't have permission to use this command.Only owner of the bot can use this command for now!`)
-        const row = new MessageActionRow()
-        .addComponents(
-            new MessageSelectMenu()
-                .setCustomId('roles')
-                .setPlaceholder('Select a reaction role')
-                .addOptions([
-                    { //edit the option according to you ⚠leave the emoji fields like they are 
-                        label: 'Reaction Role 1',
-                        description: 'Take this role by clicking me ',
-                        value: 'first_option'
-                    },
-                    {
-                        label: 'Reaction Role 2',
-                        description: 'Take this role by clicking me ',
-                        value: 'second_option'
-                    },
-                    {
-                        label: 'Reaction Role 3',
-                        description: 'Take this role by clicking me ',
-                        value: 'third_option'
-                    },
-                    {
-                        label: 'Reaction Role 4',
-                        description: 'Take this role by clicking me ',
-                        value: 'fourth_option'
-                    },
-                    {
-                        label: 'Reaction Role 5',
-                        description: 'Take this role by clicking me s',
-                        value: 'fifth_option'
-                    },
-                ]),
-        );
+        const pronRolesEmbed = new Discord.MessageEmbed()
+        .setTitle("Customize Your Pronounce Roles")
+        .setColor("#5267f6")
+        .setDescription(
+          "Select by which Pronounce you identify as by selecting the role below:"
+        )
+        const pronRolesMenu = new MessageSelectMenu()
+        .setCustomId('pronounce_roles')
+        .setPlaceholder('Select your Pronounce')
+        .addOptions([
+          {
+            label: 'He/Him',
+            description: 'Let people know your He/Him!',
+            value: 'role1'
+          },
+          {
+            label: 'She/Her',
+            description: 'Let people know your She/Her!',
+            value: 'role2'
+          },
+          {
+            label: 'Ask Pronounce',
+            description: 'Select this if you want ppl to ask your pronounce!',
+            value: 'role3'
+          },
+          {
+            label: 'They/Them',
+            description: 'Let people know your They/Them!',
+            value: 'role3'
+          },
+        ])
         const channel = interaction.client.channels.cache.get("852791356664709160")
         if(interaction.user.id === '754381104034742415'){
             if(!channel){
                 interaction.reply(`I don't have access to that channel!`)
             }else{
-                channel.send({  content: "Hello There take your roles",  ephemeral: true ,components: [row]})
+                channel.send({ embeds: [pronRolesEmbed], components: [{ type: 1, components: [pronRolesMenu] }] })
                 interaction.reply('Done!')
             }
         }else {
