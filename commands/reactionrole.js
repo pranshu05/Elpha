cd33211cbb0ff9299166a172554809b3a7056c0a
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-13 17:50:04 +0530

    nnn

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const  Interaction  = require("discord.js")
 const Discord = require('discord.js')
-const {MessageActionRow,  MessageButton,  ButtonInteraction, MessageSelectMenu, } = require('discord.js')
+const {Client, Intents, MessageActionRow, MessageSelectMenu } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("reaction_roles")
@@ -11,43 +11,50 @@ module.exports = {
         .setColor('#FF0000')
 	    .setTitle(`**:x: Insufficient Permission!**`)
         .setDescription(`You don't have permission to use this command.Only owner of the bot can use this command for now!`)
-        const pronRolesEmbed = new Discord.MessageEmbed()
-        .setTitle("Customize Your Pronounce Roles")
-        .setColor("#5267f6")
-        .setDescription(
-          "Select by which Pronounce you identify as by selecting the role below:"
-        )
-        const pronRolesMenu = new MessageSelectMenu()
-        .setCustomId('pronounce_roles')
-        .setPlaceholder('Select your Pronounce')
-        .addOptions([
-          {
-            label: 'He/Him',
-            description: 'Let people know your He/Him!',
-            value: 'test',
-          },
-          {
-            label: 'She/Her',
-            description: 'Let people know your She/Her!',
-            value: 'test2',
-          },
-          {
-            label: 'They/Them',
-            description: 'Select this if you want ppl to ask your pronounce!',
-            value: 'test3',
-          },
-          {
-            label: 'Ask Pronounce',
-            description: 'Let people know your They/Them!',
-            value: 'test3',
-          },
-        ])
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageSelectMenu()
+                .setCustomId('roles')
+                .setPlaceholder('Select a reaction role')
+                .addOptions([
+                    { //edit the option according to you ⚠leave the emoji fields like they are 
+                        label: 'Reaction Role 1',
+                        description: 'Take this role by clicking me ',
+                        value: 'first_option',
+                        emoji: r1m
+                    },
+                    {
+                        label: 'Reaction Role 2',
+                        description: 'Take this role by clicking me ',
+                        value: 'second_option',
+                        emoji: r2m
+                    },
+                    {
+                        label: 'Reaction Role 3',
+                        description: 'Take this role by clicking me ',
+                        value: 'third_option',
+                        emoji: r3m
+                    },
+                    {
+                        label: 'Reaction Role 4',
+                        description: 'Take this role by clicking me ',
+                        value: 'fourth_option',
+                        emoji: r4m
+                    },
+                    {
+                        label: 'Reaction Role 5',
+                        description: 'Take this role by clicking me s',
+                        value: 'fifth_option',
+                        emoji: r5m
+                    },
+                ]),
+        );
         const channel = interaction.client.channels.cache.get("852791356664709160")
         if(interaction.user.id === '754381104034742415'){
             if(!channel){
                 interaction.reply(`I don't have access to that channel!`)
             }else{
-                channel.send({ embeds: [pronRolesEmbed], components: [{ type: 1, components: [pronRolesMenu] }] })
+                channel.send({  content: "Hello There take your roles",  ephemeral: true ,components: [row]})
                 interaction.reply('Done!')
             }
         }else {
