a07e07072c7d0143c05cb668a5674ac5bc6f9c2c
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-13 19:09:48 +0530

    try this or delete

--- bot.js
+++ bot.js
@@ -71,4 +71,148 @@ client.on('guildDelete' , guild => {
     }
         
     })
+client.on('interactionCreate' , interaction => {
+    
+    if(interaction.isSelectMenu()){
+        const choice = interaction.values[0] 
+        const member = interaction.member
+        if(interaction.customID === 'roles') {
+            console.log('some1 reacted!')
+            if(choice === '1'){
+                if (member.roles.cache.has('959447841355468801')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447841355468801')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447841355468801')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '2'){
+                if (member.roles.cache.has('959447841355468801')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447841355468801')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447841355468801')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '3'){
+                if (member.roles.cache.has('959447842282414130')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447842282414130')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447842282414130')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '4'){
+                if (member.roles.cache.has('959447842844467210')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447842844467210')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447842844467210')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '5'){
+                if (member.roles.cache.has('959447834254532658')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447834254532658')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447834254532658')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '6'){
+                if (member.roles.cache.has('959447834669752362')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447834669752362')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447834669752362')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '7'){
+                if (member.roles.cache.has('959447835181461554')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447835181461554')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447835181461554')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '8'){
+                if (member.roles.cache.has('959447835663802368')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447835663802368')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447835663802368')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '9'){
+                if (member.roles.cache.has('959447846166343740')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447846166343740')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447846166343740')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '10'){
+                if (member.roles.cache.has('959447845700767755')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447845700767755')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447845700767755')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+            if(choice === '11'){
+                if (member.roles.cache.has('959447846745153586')) {
+                    interaction.reply({content: "The role was successfully removed from you" , ephemeral: true})
+                    member.roles.remove('959447846745153586')
+                    .catch(console.error)
+                }
+                else{
+                    member.roles.add('959447846745153586')
+                    await interaction.reply({ content: "The role was successfully added to you", ephemeral: true })
+                    .catch(console.error)
+                }
+            }
+        }else{
+            returnj
+        }
+    }
+})
 client.login(process.env.token)
