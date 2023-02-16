53cbd3752a314d4ca81604c345715b5dfe740439
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-16 21:07:33 +0530

    fixed addgif

--- commands/addgif.js
+++ commands/addgif.js
@@ -10,6 +10,7 @@ module.exports = {
     async execute(interaction) {
         const name = interaction.options.getString('name')
         const url = interaction.options.getString('url')
+        const gif =  await Gif.find({guild_id: interaction.guild.id, gif_name: name})
         const gif_success = new Discord.MessageEmbed().setColor('#00FF00')
 	    .setTitle('**:white_check_mark: GIF added**')
         .setDescription(`GIF named `+ name +` added!\n` + `type elp gif `+name +` to send gif\n` + `You can undo this by **/removegif** command`)
@@ -21,26 +22,31 @@ module.exports = {
             interaction.reply('Please provide a valid URL')
             return
         }
-        Gif.findOne({guild_id: interaction.guild.id}, (err, settings) => {
-            if(err){
-                console.log(err)
-                interaction.reply({embeds: [gif_fail]})
-                return
-            }else{
-                settings = new Gif({
-                guild_id: interaction.guild.id,
-                gif_name: name,
-                gif_url : url,                       
-                })
-            } 
-            settings.save(err => {
+        if(!gif.length === 0){
+            interaction.reply(`Gif named **${name}** already exist in this server! Please select another name.`)
+            return
+        }else{
+            Gif.findOne({guild_id: interaction.guild.id}, (err, settings) => {
                 if(err){
                     console.log(err)
-                    interaction.reply({embeds: [gif_fail]})
+                    interaction.reply({embeds: [gif_fail]}) 
                     return
-                }
+                }else{
+                    settings = new Gif({
+                    guild_id: interaction.guild.id,
+                    gif_name: name,
+                    gif_url : url,                       
+                    })
+                } 
+                settings.save(err => {
+                    if(err){
+                        console.log(err)
+                        interaction.reply({embeds: [gif_fail]})
+                        return
+                    }
+                })
             })
-        })
-        interaction.reply({embeds: [gif_success] }).catch(console.error)
+            interaction.reply({embeds: [gif_success] }).catch(console.error)
+        }
     }
 }
