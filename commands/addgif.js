75b7605d707956870a45b62c7c87b3fcc6d597ee
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-16 20:17:09 +0530

    j

--- commands/addgif.js
+++ commands/addgif.js
@@ -10,7 +10,6 @@ module.exports = {
     async execute(interaction) {
         const name = interaction.options.getString('name')
         const url = interaction.options.getString('url')
-        const gif = Gif.findOne({guild_id: interaction.guild.id , gif_name: name})
         const gif_success = new Discord.MessageEmbed().setColor('#00FF00')
 	    .setTitle('**:white_check_mark: GIF added**')
         .setDescription(`GIF named `+ name +` added!\n` + `type elp gif `+name +` to send gif\n` + `You can undo this by **/removegif** command`)
@@ -22,11 +21,7 @@ module.exports = {
             interaction.reply('Please provide a valid URL')
             return
         }
-        if(gif.length !== 0){
-            interaction.reply(`Gif named **${name}** already exist in this server! Please select another name.`)
-            return
-        }else{
-            Gif.findOne({guild_id: interaction.guild.id}, (err, settings) => {
+        Gif.findOne({guild_id: interaction.guild.id}, (err, settings) => {
             if(err){
                 console.log(err)
                 interaction.reply({embeds: [gif_fail]})
@@ -46,7 +41,6 @@ module.exports = {
                 }
             })
         })
-        interaction.reply({embeds: [gif_success]}).catch(console.error)
-        }
+        interaction.reply({embeds: [gif_success] }).catch(console.error)
     }
 }
