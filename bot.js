7deab094f4cf03448f3fef73976cdcc82f76f0c0
slekup <74949101+slekup@users.noreply.github.com>
2023-02-21 08:11:11 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const fs = require('fs');
-const { Client, Collection } = require('discord.js');
+const { Client, Collection, MessageEmbed } = require('discord.js');
 const Database = require('./config/Database');
 const db = new Database();
 db.connect();
+
 const client = new Client({
   intents: [
     'GUILDS',
@@ -18,20 +19,24 @@ const client = new Client({
     'GUILD_VOICE_STATES',
   ],
 });
-const Discord = require('discord.js');
+
 const commandFiles = fs
   .readdirSync('./commands')
   .filter((file) => file.endsWith('.js'));
+
 const commands = [];
 client.commands = new Collection();
+
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
   client.commands.set(command.data.name, command);
 }
+
 const eventFiles = fs
   .readdirSync('./events')
   .filter((file) => file.endsWith('.js'));
+
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   if (event.once) {
@@ -40,8 +45,9 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args, commands));
   }
 }
+
 client.on('guildCreate', (guild) => {
-  const guild_embed = new Discord.MessageEmbed()
+  const guild_embed = new MessageEmbed()
     .setColor('00FFFF')
     .setTitle('New Guild!')
     .setDescription(`Elpha has joined the server ${guild.name}`)
@@ -56,28 +62,32 @@ client.on('guildCreate', (guild) => {
     )
     .setThumbnail(guild.iconURL())
     .setTimestamp();
-  client.channels.cache
-    .get('919799899929841694')
-    .send({ embeds: [guild_embed] });
+
+  const channel = client.channels.cache.get('919799899929841694');
+  channel.send({ embeds: [guild_embed] });
 });
+
 client.on('guildDelete', (guild) => {
-  if (guild.available) {
-    const embed = new Discord.MessageEmbed()
-      .setColor('00FFFF')
-      .setTitle('Guild Left')
-      .setDescription(`Elpha has left the server ${guild.name}`)
-      .addFields(
-        { name: 'Members:', value: `${guild.memberCount}` },
-        { name: 'Guild ID:', value: `${guild.id}` },
-        {
-          name: 'Guild owner:',
-          value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``,
-        },
-        { name: 'Total servers:', value: `${client.guilds.cache.size}` }
-      )
-      .setThumbnail(guild.iconURL())
-      .setTimestamp();
-    client.channels.cache.get('919799899929841694').send({ embeds: [embed] });
-  }
+  if (!guild.available) return;
+
+  const embed = new MessageEmbed()
+    .setColor('00FFFF')
+    .setTitle('Guild Left')
+    .setDescription(`Elpha has left the server ${guild.name}`)
+    .addFields(
+      { name: 'Members:', value: `${guild.memberCount}` },
+      { name: 'Guild ID:', value: `${guild.id}` },
+      {
+        name: 'Guild owner:',
+        value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``,
+      },
+      { name: 'Total servers:', value: `${client.guilds.cache.size}` }
+    )
+    .setThumbnail(guild.iconURL())
+    .setTimestamp();
+
+  const channel = client.channels.cache.get('919799899929841694');
+  channel.send({ embeds: [embed] });
 });
+
 client.login(process.env.token);
