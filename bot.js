bade7978e742faacaeb5f78539fd59c6bf6f297c
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-26 14:56:02 +0530

    l

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const fs = require('fs')
-const { Client, Collection, MessageEmbed } = require('discord.js')
+const {Client,Collection} = require('discord.js')
 const Database = require('./config/Database')
 const db = new Database()
 db.connect()
-
-const client = new Client({
+const client = new Client({ 
     intents: [
         'GUILDS',
         'GUILD_MEMBERS',
@@ -17,77 +16,56 @@ const client = new Client({
         'GUILD_INTEGRATIONS',
         'GUILD_INVITES',
         'GUILD_VOICE_STATES',
-    ],
+    ] 
 })
-
-const commandFiles = fs
-    .readdirSync('./commands')
-    .filter((file) => file.endsWith('.js'))
-
+const Discord = require('discord.js')
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 const commands = []
 client.commands = new Collection()
-
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`)
+	const command = require(`./commands/${file}`)
     commands.push(command.data.toJSON())
-    client.commands.set(command.data.name, command)
+	client.commands.set(command.data.name, command)
 }
-
-const eventFiles = fs
-    .readdirSync('./events')
-    .filter((file) => file.endsWith('.js'))
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`)
-    if (event.once) {
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for(const file of eventFiles){
+	const event = require(`./events/${file}`)
+    if (event.once){
         client.once(event.name, (...args) => event.execute(...args, commands))
     } else {
         client.on(event.name, (...args) => event.execute(...args, commands))
     }
 }
-
-client.on('guildDelete', (guild) => {
-    // Being sent to Elpha server
-    const embed = new MessageEmbed()
-        .setColor('00FFFF')
+client.on('guildCreate' , guild => {   
+    const guild_embed = new Discord.MessageEmbed()
+    .setColor('FFFF00')
+    .setTitle('New Guild!')
+    .setDescription(`I have joined the server ${guild.name}`)
+    .addFields(
+        {name: 'Members:', value: `${guild.memberCount}`},
+        {name: 'Guild ID:', value: `${guild.id}`},
+        {name: 'Guild owner:', value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``},
+        {name: 'Total servers:', value: `${client.guilds.cache.size}`},
+    )
+    .setThumbnail(guild.iconURL())
+    .setTimestamp()
+    client.channels.cache.get('919799899929841694').send({embeds: [guild_embed]})
+})
+client.on('guildDelete' , guild => {
+    if(guild.available){
+        const embed = new Discord.MessageEmbed()
+        .setColor('FFFF00')
         .setTitle('Guild Left')
-        .setDescription(`Elpha has left the server ${guild.name}`)
+        .setDescription(`I left the server ${guild.name}`)
         .addFields(
-            { name: 'Members:', value: `${guild.memberCount}` },
-            { name: 'Guild ID:', value: `${guild.id}` },
-            {
-                name: 'Guild owner:',
-                value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``,
-            },
-            { name: 'Total servers:', value: `${client.guilds.cache.size}` }
+            {name: 'Members:', value: `${guild.memberCount}`},
+            {name: 'Guild ID:', value: `${guild.id}`},
+            {name: 'Guild owner:', value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``},
+            {name: 'Total servers:', value: `${client.guilds.cache.size}`},
         )
         .setThumbnail(guild.iconURL())
         .setTimestamp()
-
-    const channel = client.channels.cache.get('919799899929841694')
-    channel.send({ embeds: [embed] })
+        client.channels.cache.get('919799899929841694').send({embeds: [embed]})	
+    }
 })
-
-client.on('guildCreate', (guild) => {
-    // Being sent to Elpha Server
-    const guild_embed = new MessageEmbed()
-        .setColor('00FFFF')
-        .setTitle('New Guild!')
-        .setDescription(`Elpha has joined the server ${guild.name}`)
-        .addFields(
-            { name: 'Members:', value: `${guild.memberCount}` },
-            { name: 'Guild ID:', value: `${guild.id}` },
-            {
-                name: 'Guild owner:',
-                value: `> <@${guild.ownerId}> \`[${guild.ownerId}]\``,
-            },
-            { name: 'Total servers:', value: `${client.guilds.cache.size}` }
-        )
-        .setThumbnail(guild.iconURL())
-        .setTimestamp()
-
-    const new_channel = client.channels.cache.get('919799899929841694')
-    new_channel.send({ embeds: [guild_embed] })
-})
-
 client.login(process.env.token)
