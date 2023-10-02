8cc46f5823182272383c4224fee1da7d8056702c
atheryx <krishna10032004@gmail.com>
2023-10-02 23:40:22 +0530

    feat: avatar command

---
+++ src/commands/avatar.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("avatar")
+        .setDescription("Display the avatar of the user")
+        .addUserOption((option) =>
+            option
+                .setName(`user`)
+                .setDescription(`Select a user`)
+                .setRequired(false)
+        ),
+    async execute(interaction) {
+        const userMention =
+            interaction.options.getUser(`user`) || interaction.user;
+        const avatar = userMention.displayAvatarURL({
+            size: 1024,
+            extension: "png",
+        });
+
+        const embed = new EmbedBuilder()
+            .setColor("#00ffff")
+            .setAuthor({
+                name: `${userMention.tag}'s avatar`,
+                iconURL: `${userMention.displayAvatarURL()}`,
+            })
+            .setTitle(`Download`)
+            .setURL(avatar)
+            .setImage(avatar);
+
+        const message = await interaction.reply({ embeds: [embed] });
+    },
+};
