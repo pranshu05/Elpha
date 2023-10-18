97f142891067db26833d3453487cb444e25f50d6
Shruti0316 <shruti16.k@gmail.com>
2023-10-18 17:53:53 +0530

    feat: choices command

---
+++ src/commands/choice.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("choose")
+        .setDescription("Elpha will choose for you!")
+        .addSubcommand(subcommand => subcommand
+            .setName("choices")
+            .setDescription('choice')
+            .addStringOption(option => option
+                .setName('first')
+                .setDescription('Enter the first choice')
+                .setRequired(true)
+            )
+            .addStringOption(option => option
+                .setName('second')
+                .setDescription('Enter the second choice')
+                .setRequired(true)
+            )
+        ),
+    async execute(interaction) {
+        const firstChoice = interaction.options.getString('first');
+        const secondChoice = interaction.options.getString('second');
+
+        const choose = [firstChoice, secondChoice];
+        const chosenIndex = Math.floor(Math.random() * choose.length);
+
+        const embed = new MessageEmbed()
+            .setColor('00FFFF')
+            .setTitle('I choose ' + choices[chosenIndex])
+            .addFields(
+                { name: 'First choice', value: firstChoice },
+                { name: 'Second choice', value: secondChoice }
+            )
+            .setFooter(`Chosen for ${interaction.user.username}`);
+
+        await interaction.reply({ embeds: [embed] }).catch(console.error);
+    }
+};
